2f7fd015b9c2ecf38af51051e918ed7dc99bc664
kellingraywilliamson <55054332+kellingraywilliamson@users.noreply.github.com>
2022-08-16 22:36:06 +0100

    Added basic animations

--- Assets/Scripts/Mover.cs
+++ Assets/Scripts/Mover.cs
@@ -5,27 +5,35 @@ namespace GDTV
 {
     public class Mover : MonoBehaviour
     {
-        [SerializeField] private Transform target;
-
         private Ray _lastRay;
         private NavMeshAgent _agent;
+        private Animator _animator;
         private Camera _camera;
+        private static readonly int ForwardSpeed = Animator.StringToHash("forwardSpeed");
 
         private void Start()
         {
             _camera = Camera.main;
             _agent = GetComponent<NavMeshAgent>();
+            _animator = GetComponent<Animator>();
         }
 
         private void Update()
         {
             if (Input.GetMouseButtonDown(0)) MoveToCoordinates();
+            UpdateAnimator();
         }
 
         private void MoveToCoordinates()
         {
             var ray = _camera.ScreenPointToRay(Input.mousePosition);
             if (Physics.Raycast(ray, out var hit)) _agent.SetDestination(hit.point);
+        }
+
+        private void UpdateAnimator()
+        {
+            var localVelocity = transform.InverseTransformDirection(_agent.velocity);
+            _animator.SetFloat(ForwardSpeed, localVelocity.z);
         }
     }
 }
